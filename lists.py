240fbe5e7cc9f9d8e9e27a49316e5dcc6026339a
Sergey Lisin <sergey.lisin@gmail.com>
2021-02-17 19:02:41 +0300

    delete all

--- lists.py
+++
@@ -1,6 +0,0 @@
-d = {'city': 'Москва', 'temperature': '20'}
-print(d['city'])
-d['temperature'] = str(int(d['temperature'])-5)
-print(d)
-print(d.get('country', 'Россия'))
-d['date'] = '27.05.2019'
--- price.py
+++
@@ -1,9 +0,0 @@
-def format_price(price):
-    p = int(price)
-    ret = f'Цена: {p} руб.'
-    return ret
-
-price = 56.24
-formatted_price = format_price(price)
-print(formatted_price)
-
